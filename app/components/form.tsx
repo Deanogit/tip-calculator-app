b41ad5cb55c10200881734f5f434577abf6f07e1
Dean Howe <hideanhowe@gmail.com>
2025-08-18 20:32:01 +0100

    mobile icons

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Form() {
   return (
     <>
@@ -8,29 +10,39 @@ export default function Form() {
         >
           <h1 className="mb-2">Bill</h1>
         </label>
-        <input
-          type="text"
-          placeholder="0"
-          className="cursor-pointer bg-[var(--cust-grey-50)] w-full preset-3 px-4 mb-8 rounded-[5px] text-right"
-        />
+        <div className="flex items-center relative">
+          <Image
+            src="/images/icon-dollar.svg"
+            width={11}
+            height={17}
+            alt=""
+            className="absolute top-2 left-4 translate-y-1/8 z-10"
+          />
+          <input
+            type="text"
+            placeholder="0"
+            className="cursor-pointer bg-[var(--cust-grey-50)] w-full preset-3 px-4 mb-8 rounded-[5px] text-right text-[var(--cust-green-900)] relative"
+            required
+          />
+        </div>
         <div className="text-[var(--cust-grey-500)]">
           <div className="grid grid-cols-2 gap-4 mb-8">
             <div className="col-span-2">
               <h2 className="preset-5">Select tip %</h2>
             </div>
-            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3">
+            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3 hover:bg-[var(--cust-green-400)] cursor-pointer hover:text-[var(--cust-green-900)]">
               5%
             </button>
-            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3">
+            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3 hover:bg-[var(--cust-green-400)] cursor-pointer hover:text-[var(--cust-green-900)]">
               10%
             </button>
-            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3">
+            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3 hover:bg-[var(--cust-green-400)] cursor-pointer hover:text-[var(--cust-green-900)]">
               15%
             </button>
-            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3">
+            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3 hover:bg-[var(--cust-green-400)] cursor-pointer hover:text-[var(--cust-green-900)]">
               25%
             </button>
-            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3">
+            <button className="bg-[var(--cust-green-900)] rounded-[5px] text-white preset-3 hover:bg-[var(--cust-green-400)] cursor-pointer hover:text-[var(--cust-green-900)]">
               50%
             </button>
             <input
@@ -42,13 +54,23 @@ export default function Form() {
           <label htmlFor="people">
             <h3 className="mb-2">Number of People</h3>
           </label>
-          <input
-            type="text"
-            name="people"
-            id="people"
-            placeholder="0"
-            className="cursor-pointer bg-[var(--cust-grey-50)] w-full preset-3 px-4 mb-8 rounded-[5px] text-right"
-          />
+          <div className="relative flex items-center">
+            <Image
+              src="/images/icon-person.svg"
+              width={13}
+              height={16}
+              alt=""
+              className="absolute top-2 left-4 translate-y-1/8"
+            />
+            <input
+              type="text"
+              name="people"
+              id="people"
+              placeholder="0"
+              className="cursor-pointer bg-[var(--cust-grey-50)] w-full preset-3 px-4 mb-8 rounded-[5px] text-right text-[var(--cust-green-900)]"
+              required
+            />
+          </div>
         </div>
       </form>
     </>
